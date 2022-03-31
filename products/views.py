787909bb7b2c0845dd146d4a4e4ab4cb15eeff5c
Michael J <johnson_michael1329@yahoo.com>
2022-03-31 14:17:48 -0500

    Created function to get product by id in the views.py file + added urls to represent the unique data requested.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
+
 
 @api_view(['GET', 'POST'])
 def product_list(request):
@@ -18,4 +20,13 @@ def product_list(request):
   else:
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:    
+        return Response(status=status.HTTP_404_NOT_FOUND)
 # Create your views here.
