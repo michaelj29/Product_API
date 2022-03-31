726f7b0c711818bffd3918319515ffc84d8a02aa
Michael J <johnson_michael1329@yahoo.com>
2022-03-31 13:59:55 -0500

    Created POST functionality to the views.py file.

--- products/views.py
+++ products/views.py
@@ -1,15 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-from products import serializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
   product = Product.objects.all()
-  serializer = ProductSerializer(product, many=True)
-  return Response(serializer.data)
-
-
+  if request.method == 'GET':
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
