f903e3a36e5232c25c110c25516c0e153e6ec353
Michael J <johnson_michael1329@yahoo.com>
2022-03-31 11:19:29 -0500

    Updated product urls + updated product_project urls + created first view function.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def product_list(request):
+
+  return Response('ok')
+
+
 
 # Create your views here.
